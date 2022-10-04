6065a180bfa05194fe2100e59dccfaffa34fe9fc
Hasan Çelik <hsn.2903@outlook.com>
2022-10-04 07:47:41 +0300

    add header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,49 @@
 import React from "react";
+import { IoBookmark, IoChatbox, IoSearchOutline } from "react-icons/io5";
+
+import logo from "../assets/img/logo.png";
+import userPhoto from "../assets/img/user.jpg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <header class="header">
+        <img src={logo} alt="trillo logo" class="logo" />
+
+        <form action="#" class="search">
+          <input
+            type="text"
+            class="search__input"
+            placeholder="Search hotels"
+          />
+          <button class="search__button">
+            <IoSearchOutline class="search__icon" />
+          </button>
+        </form>
+
+        <nav class="user-nav">
+          <div class="user-nav__icon-box">
+            <IoBookmark className="user-nav__icon" />
+            <span class="user-nav__notification">7</span>
+          </div>
+          <div class="user-nav__icon-box">
+            <IoChatbox className="user-nav__icon" />
+            <span class="user-nav__notification">13</span>
+          </div>
+          <div class="user-nav__user">
+            <img
+              src={userPhoto}
+              alt="User photo"
+              class="user-nav__user-photo"
+            />
+            <span class="user-nav__user-name">Jonas</span>
+          </div>
+        </nav>
+      </header>
+      <nav className="sidebar">sidebar</nav>
+      <div className="content">hotel view</div>
+    </div>
+  );
 };
 
 export default Home;
