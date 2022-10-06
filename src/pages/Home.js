2f8db46a014b3f664741381498c604a976036f4c
Hasan Çelik <hsn.2903@outlook.com>
2022-10-06 06:43:32 +0300

    update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { IoBookmark, IoChatbox, IoSearchOutline } from "react-icons/io5";
+import {
+  IoBookmark,
+  IoChatbox,
+  IoSearchOutline,
+  IoHomeSharp,
+  IoAirplaneSharp,
+  IoKeySharp,
+  IoMapSharp,
+  IoStarSharp,
+  IoLocationSharp,
+} from "react-icons/io5";
 
 import logo from "../assets/img/logo.png";
 import userPhoto from "../assets/img/user.jpg";
+
+import hotel1 from "../assets/img/hotel-1.jpg";
+import hotel2 from "../assets/img/hotel-2.jpg";
+import hotel3 from "../assets/img/hotel-3.jpg";
 
 const Home = () => {
   return (
@@ -40,8 +54,73 @@ const Home = () => {
           </div>
         </nav>
       </header>
-      <nav className="sidebar">sidebar</nav>
-      <div className="content">hotel view</div>
+      <nav className="sidebar">
+        <ul className="sidenav">
+          <li className="sidenav__item sidenav__item--active">
+            <a href="#" className="sidenav__link">
+              <IoHomeSharp className="sidenav__icon" />
+              <span>Hotel</span>
+            </a>
+          </li>
+          <li className="sidenav__item">
+            <a href="#" className="sidenav__link">
+              <IoAirplaneSharp className="sidenav__icon" />
+              <span>Flight</span>
+            </a>
+          </li>
+          <li className="sidenav__item">
+            <a href="#" className="sidenav__link">
+              <IoKeySharp className="sidenav__icon" />
+              <span>Car Rental</span>
+            </a>
+          </li>
+          <li className="sidenav__item">
+            <a href="#" className="sidenav__link">
+              <IoMapSharp className="sidenav__icon" />
+              <span>Tours</span>
+            </a>
+          </li>
+        </ul>
+
+        <div className="legal">
+          &copy; {new Date().getFullYear()} by Trillo. All rights reserved.
+        </div>
+      </nav>
+      <div className="content">
+        <div class="gallery">
+          <figure class="gallery__item">
+            <img src={hotel1} alt="Photo of hotel 1" class="gallery__photo" />
+          </figure>
+          <figure class="gallery__item">
+            <img src={hotel2} alt="Photo of hotel 2" class="gallery__photo" />
+          </figure>
+          <figure class="gallery__item">
+            <img src={hotel3} alt="Photo of hotel 3" class="gallery__photo" />
+          </figure>
+        </div>
+
+        <div class="overview">
+          <h1 class="overview__heading">Hotel Las Palmas</h1>
+
+          <div class="overview__stars">
+            <IoStarSharp class="overview__icon-star" />
+            <IoStarSharp class="overview__icon-star" />
+            <IoStarSharp class="overview__icon-star" />
+            <IoStarSharp class="overview__icon-star" />
+            <IoStarSharp class="overview__icon-star" />
+          </div>
+
+          <div class="overview__location">
+            <IoLocationSharp class="overview__icon-location" />
+            <button class="btn-inline">Albufeira, Portugal</button>
+          </div>
+
+          <div class="overview__rating">
+            <div class="overview__rating-average">8.6</div>
+            <div class="overview__rating-count">429 votes</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
